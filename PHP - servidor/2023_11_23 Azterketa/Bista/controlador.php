67c56588972d566918c6143a7db713d7df59237d
osdomz <osdom.dev@hotmail.com>
2023-12-05 14:29:35 +0100

    update php olentzero

--- PHP - servidor/2023_11_23 Azterketa/Bista/controlador.php
+++ PHP - servidor/2023_11_23 Azterketa/Bista/controlador.php
@@ -6,63 +6,89 @@ include_once 'bista.php';
 include_once 'modelo.php';
 
 $Vista = new Login_Bista;
-
 $modelo = new Model;
 $modelo->conectar();
 
-// Comprobar el inicio de sesión, mostrar un mensaje de error y cargar el formulario nuevamente si no es válido.
-if (isset($_POST['accion']) && !$_SESSION["validado"]) {
-    if ($modelo->validarInicioSesion($_POST['erab_usuario'], $_POST['password_usuario'])) {
-        $_SESSION["validado"] = true;
-        $_SESSION["Usuario"] = $_POST['erab_usuario'];
+if (isset($_POST['accion'])) {
+    switch ($_POST['accion']) {
+        case "entrar":
+            if (!$_SESSION["validado"]) {
+                // Validar el inicio de sesión
+                if ($modelo->validarInicioSesion($_POST['erab_usuario'], $_POST['password_usuario'])) {
+                    $_SESSION["validado"] = true;
+                    $_SESSION["Usuario"] = $_POST['erab_usuario'];
+                    echo '<h3 style="color: green;">Bienvenido ' . $_POST['erab_usuario'] . ' </h3>';
         
-        echo'Usuario validado;)';
-        $Vista->Login();
-    } else {
-        ?>
-        <h3 style="color: red;">Inténtalo de nuevo, el usuario o la contraseña no son válidos.</h3>
-        <?php
-       $Vista->Login();
-    }
-    if ($_SESSION["validado"] && isset($_POST['accion'])) {
-        if (isset($_POST['accion'])) {
-            switch ($_POST['accion']) {
-                case "cambiar":
-                    $Vista->ikusiPasahitzaAldatzeko_verCambioContras();
-                    break;
-                case "aldatu":
-                    // Check if required data is set
-                    if (isset($_POST['erab_usuario']) && isset($_POST['password_usuario'])) {
-                        $modelo->actualizarPass($_POST['erab_usuario'], $_POST['password_usuario']);
-                        echo "Contraseña actualizada con éxito";
+                    // Obtener el valor de olentzero_MariDomingi desde la base de datos
+                    $olentzero_MariDomingi = $modelo->balioztatuOlentzero($_SESSION['Usuario']);
+        
+                    // Redirigir a la vista correspondiente
+                    if ($olentzero_MariDomingi == 0) {
+                        $Vista->AukeraEmanErab_DarOpcionesUsuario();
+                    } elseif ($olentzero_MariDomingi == 1) {
+                        $Vista->AukeraEmanOlen_DarOpcionesOlen($modelo->filtrarUsuarios());
                     } else {
-                        echo "Error: Datos incompletos";
+                        echo '<h3 style="color: red;">Error al obtener la información del usuario.</h3>';
                     }
-                    break;
-                case "registrar":
-                    $Vista->Alta_AukeraEman_Opcion();
-                case"Ok":
-                    $Vista = new Login_Bista;
-                    $nombre = $_POST["izena_nombre"];
-                    $usuario = $_POST["erab_usuario"];
-                    $password = $_POST["ph"];
-                    $fechaNac = date('Y-m-d H:i:s');
         
-                    $resultado = $modelo->agregarUsuario($nombre, $usuario, $password, $fechaNac);
-        
-                    if ($resultado) {
-                        echo "Usuario añadido con éxito";
-                    } else {
-                        echo "Error al añadir usuario";
-                    }
-                    break;             
+                } else {
+                    echo '<h3 style="color: red;">Inténtalo de nuevo, el usuario o la contraseña no son válidos.</h3>';
+                    $Vista->Login();
+                }
+            } else {
+                echo '<h3 style="color: red;">Ya estás logueado.</h3>';
+                $Vista->Login();
             }
-        } else {
-            ?>
-            <h3 style="color: red;">No has seleccionado lo que quieres hacer.</h3>
-            <?php
-            $Vista->ikusiPasahitzaAldatzeko_verCambioContras();
-        }
+            break;
+        
+
+        case "cambiar":
+            if ($_SESSION["validado"]) {
+                $Vista->ikusiPasahitzaAldatzeko_verCambioContras();
+            } else {
+                echo '<h3 style="color: red;">Debes iniciar sesión para cambiar la contraseña.</h3>';
+                $Vista->Login();
+            }
+            break;
+
+        case "aldatu":
+            if ($_SESSION["validado"]) {
+                $aldatu = true;
+                if ($aldatu) {
+                    if (isset($_POST['ph'])) {
+                        // Actualizar la contraseña en la base de datos
+                        $contrasena_actualizada = $_POST['ph'];
+                        // Aquí deberías realizar las validaciones y hashing adecuados
+                        $modelo->actualizarPass($_SESSION['Usuario'], $contrasena_actualizada);
+                        echo '<h3 style="color: green;">Contraseña actualizada correctamente.</h3>';
+                        $Vista->Login();
+                    } else {
+                        echo '<h3 style="color: red;">Error.</h3>';
+                        $Vista->Login();
+                    }
+                }
+            } else {
+                echo '<h3 style="color: red;">Debes iniciar sesión para cambiar la contraseña.</h3>';
+                $Vista->Login();
+            }
+            break;
+
+        case "registrar":
+            $Vista->Alta_AukeraEman_Opcion();
+        case "Ok":
+            $modelo->agregarUsuario(
+                $_POST['izena_nombre'],
+                $_POST['erab_usuario'],
+                $_POST['ph'],
+                $_POST['data_fecha']
+            );
+            echo '<h3 style="color: green;">Registrado correctamente.</h3>';
+            $Vista->Login();
+            break;
+
+        default:
+            echo '<h3 style="color: red;">No has seleccionado lo que quieres hacer.</h3>';
+            $Vista->Login();
+            break;
     }
 }
-
--- PHP - servidor/2023_11_23 Azterketa/Bista/modelo.php
+++ PHP - servidor/2023_11_23 Azterketa/Bista/modelo.php
@@ -1,11 +1,13 @@
 <?php
 
-class Model {
+class Model
+{
 
     private $mysqli;
 
     // Función para realizar la conexión
-    public function conectar() {
+    public function conectar()
+    {
         try {
             $this->mysqli = new mysqli('localhost', 'root', '', 'gabonak');
             if ($this->mysqli->connect_errno) {
@@ -17,31 +19,53 @@ class Model {
     }
 
     // Función para comprobar el inicio de sesión
-    public function validarInicioSesion($usuario, $contrasena) {
+    public function validarInicioSesion($usuario, $contrasena)
+    {
         $sql = "SELECT * FROM erabiltzaileak_usuarios WHERE erab_usuario = ? and pasahitza_contraseña = ?";
         $stmt = $this->mysqli->prepare($sql);
         $stmt->bind_param('si', $usuario, $contrasena);
         $stmt->execute();
         $stmt->store_result();
-        
+
         return $stmt->num_rows == 1;
     }
 
-    public function agregarUsuario($erab_usuario, $pasahitza_contraseña, $izena_nombre, $jaiotze_data_fecha_nacimiento) {
-        $sql = "INSERT INTO erabiltzaileak_usuarios (erab_usuario, pasahitza_contraseña, izena_nombre, jaiotze_data_fecha_nacimiento) VALUES (?, ?, ?, ?)";
+    // Función para agregar un nuevo usuario
+    public function agregarUsuario($nombre, $usuario, $contrasena, $fechaNac)
+    {
+        // Utilizamos NULL para permitir que el campo id_erab_usuario se autoincremente
+        $sql = "INSERT INTO erabiltzaileak_usuarios (erab_usuario, pasahitza_contraseña, izena_nombre, jaiotze_data_fecha_nacimiento, olentzero_MariDomingi, puntuazioa_puntuacion) VALUES (?, ?, ?, ?, 0, 0)";
         $stmt = $this->mysqli->prepare($sql);
-        $stmt->bind_param('sisd', $erab_usuario, $pasahitza_contraseña, $izena_nombre, $jaiotze_data_fecha_nacimiento);
+        $stmt->bind_param('ssss', $usuario, $contrasena, $nombre, $fechaNac);
+
         return $stmt->execute();
     }
 
     // Se actualizará la password en la base de datos
-
-    public function actualizarPass($usuario, $contrasena) {
-        $sql = "UPDATE erabiltzaileak_usuarios SET pasahitza_contraseña = pasahitza_contraseña + ? WHERE erab_usuario = ?";
-        $stmt = $this->mysqli->prepare($sql);
-        $stmt->bind_param('si',$usuario, $contrasena);
-        $stmt->execute();
+    public function actualizarPass($erab_usuario, $contrasena)
+    {
+        $sql = "UPDATE erabiltzaileak_usuarios SET pasahitza_contraseña = '$contrasena' WHERE erab_usuario = '$erab_usuario'";
+        return $this->mysqli->query($sql);
     }
+    public function balioztatuOlentzero($user)
+    {
 
+        $sql = "SELECT erab_usuario FROM  erabiltzaileak_usuarios WHERE erab_usuario='$user' AND olentzero_MariDomingi = '1'";
+        $this->mysqli->query($sql);
+        if ($this->mysqli->affected_rows == 1) {
+            return true;
+        } else {
+            return  false;
+        }
+    }
+    public function filtrarUsuarios()
+    {
+        $usuarios = array();
+        $sql = "SELECT erab_usuario FROM erabiltzaileak_usuarios WHERE olentzero_MariDomingi = '0'";
+        $resultado = $this->mysqli->query($sql);
+        while ($fila = $resultado->fetch_assoc()) {
+            $usuarios[] = $fila['erab_usuario'];
+        }
+        return $usuarios;
+    }
 }
-
