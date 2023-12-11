6c6dd92b7fdd527ed433b6eabdab14b6f91b21cc
osdomz <osdom.dev@hotmail.com>
2023-12-11 12:05:57 +0100

    update MFROGA

--- Desarrollo e interfaces web/MFROGA 21_22 MENUS a JSON ikasleentzat/JS/modal.js
+++ Desarrollo e interfaces web/MFROGA 21_22 MENUS a JSON ikasleentzat/JS/modal.js
@@ -116,19 +116,20 @@ function pillarDatos() {
   let aqui = document.getElementById("hemenJarri");
   let texto = "";
   let total = 0;
+  var btnComprar = document.getElementById("btnComprar");
 
+  btnComprar.setAttribute("disabled", "disabled");
+  
   platosSeleccionados.forEach(function (plato) {
-    if (platosSeleccionados.length > 0) {
-    }
     texto +=
       plato.getAttribute("data-titulo") + " " + plato.value + "€" + "<br>";
     total += parseFloat(plato.value);
-
-    aqui.innerHTML = texto + "<br>" + "Total: " + total + "€";
-    platosSeleccionados = true;
   });
+
+  aqui.innerHTML = texto + "<br>" + "Total: " + total + "€";
   alert("Has seleccionado " + platosSeleccionados.length + " platos");
 }
+
 function filtroEdad(event) {
   if (event.key >= 0 && event.key <= 9) {
     return true;
@@ -143,13 +144,27 @@ document.getElementById("edad").addEventListener("keypress", function (event) {
   }
 });
 
-document.getElementById("edad").addEventListener("focusout", function () {
+document.getElementById("edad").addEventListener("keyup", function () {
   var edad = document.getElementById("edad").value;
   var direccion = document.getElementById("direccion");
 
-  if (edad.length > 2) {
-    direccion.setAttribute("disabled", false);
+  if (edad.length >= 2) {
+    direccion.removeAttribute("disabled");
   } else {
-    direccion.removeAttribute("disabled", true);
+    direccion.setAttribute("disabled", "disabled");
   }
 });
+
+document.getElementById("direccion").addEventListener("keyup", function () {
+  var direccion = document.getElementById("direccion").value;
+  var btnComprar = document.getElementById("btnComprar");
+
+  if (direccion.length > 5) {
+    btnComprar.removeAttribute("disabled");
+  } else {
+    btnComprar.setAttribute("disabled", "disabled");
+  }
+});
+
+
+
